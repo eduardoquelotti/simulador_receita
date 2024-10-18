1f76cd7cb0686d2431fdbf92b721ece07cad01c4
Eduardo Quelotti <eduardoquelotti@gmail.com>
2024-10-18 00:03:05 -0300

    commit final

--- simulador_multiplo.py
+++ simulador_multiplo.py
@@ -448,5 +448,3 @@ df_receita_transposta_formatado = df_receita_transposta.applymap(moeda_format)
 # Exibir a tabela de receita transposta
 st.markdown("#### Receita por Tipo de Reconhecimento")
 st.dataframe(df_receita_transposta_formatado)
-
-t
